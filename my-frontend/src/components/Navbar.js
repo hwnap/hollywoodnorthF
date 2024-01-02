1c5eddfad3c6b2d932aa2f74faa600bf05d6600d
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-01 19:48:12 -0500

    Revert "Navbar update"
    This reverts commit 680095aee2d1056ff0f12c4d71b0bcf67ef84382.

--- my-frontend/src/components/Navbar.js
+++ my-frontend/src/components/Navbar.js
@@ -1,47 +1,26 @@
-import React, { useState } from 'react';
+import React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import IconButton from '@mui/material/IconButton';
-import MenuIcon from '@mui/icons-material/Menu';
-import MenuItem from '@mui/material/MenuItem';
-import Menu from '@mui/material/Menu';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
 import SearchIcon from '@mui/icons-material/Search';
-import LinkIcon from '@mui/icons-material/Link';
-import ArchiveIcon from '@mui/icons-material/Archive';
 
-function Navbar({ onAddTire, onSearchTire, onArchive }) {
+function Navbar({ onAddTire, onSearchTire }) {
+    // URL of the company logo
     const logoUrl = 'https://i.postimg.cc/MpCNtPX6/Whats-App-Image-2023-12-27-at-4-24-00-PM.jpg';
-    const imageUploadUrl = 'https://postimages.org/';
-
-    const [anchorEl, setAnchorEl] = useState(null);
-    const isMenuOpen = Boolean(anchorEl);
-
-    const handleMenuOpen = (event) => {
-        setAnchorEl(event.currentTarget);
-    };
-
-    const handleMenuClose = () => {
-        setAnchorEl(null);
-    };
 
     return (
-        <AppBar position="static" style={{ backgroundColor: 'white' }}>
+        <AppBar position="static" style={{ backgroundColor: 'orange' }}>
             <Toolbar style={{ justifyContent: 'space-between', alignItems: 'center' }}>
 
-                <IconButton style={{ color: 'orange' }} edge="start" onClick={handleMenuOpen}>
-                    <MenuIcon />
+                <IconButton color="inherit" onClick={onAddTire}>
+                    <AddCircleIcon />
                 </IconButton>
 
-                <Menu anchorEl={anchorEl} anchorOrigin={{ vertical: 'top', horizontal: 'right' }} keepMounted transformOrigin={{ vertical: 'top', horizontal: 'right' }} open={isMenuOpen} onClose={handleMenuClose}>
-                    <MenuItem onClick={onAddTire}><AddCircleIcon style={{ marginRight: '10px', color: 'orange' }}/> Add Tire</MenuItem>
-                    <MenuItem onClick={() => window.open(imageUploadUrl, '_blank')}><LinkIcon style={{ marginRight: '10px', color: 'orange' }}/> Image Upload</MenuItem>
-                    <MenuItem onClick={onArchive}><ArchiveIcon style={{ marginRight: '10px', color: 'orange' }}/> Archive</MenuItem>
-                </Menu>
-
+                {/* Company Logo with adjusted size */}
                 <img src={logoUrl} alt="Company Logo" style={{ maxHeight: '50px', minHeight: '20px' }} />
 
-                <IconButton style={{ color: 'orange' }} onClick={onSearchTire}>
+                <IconButton color="inherit" onClick={onSearchTire}>
                     <SearchIcon />
                 </IconButton>
 

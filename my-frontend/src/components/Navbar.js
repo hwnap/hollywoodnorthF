7141690161de3272911d69c9546021d19accf6fe
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-01 21:10:27 -0500

    Navbar update 2

--- my-frontend/src/components/Navbar.js
+++ my-frontend/src/components/Navbar.js
@@ -1,4 +1,3 @@
-// src/components/Navbar.js
 import React, { useState } from 'react';
 import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
@@ -7,13 +6,17 @@ import MenuIcon from '@mui/icons-material/Menu';
 import MenuItem from '@mui/material/MenuItem';
 import Menu from '@mui/material/Menu';
 import AddCircleIcon from '@mui/icons-material/AddCircle';
+import SearchIcon from '@mui/icons-material/Search';
 import LinkIcon from '@mui/icons-material/Link';
-import { orange } from '@mui/material/colors';
+import ArchiveIcon from '@mui/icons-material/Archive';
 
-function Navbar({ onAddTire, onOpenImageUpload }) {
+function Navbar({ onAddTire, onSearchTire, onArchive }) {
+    // URL of the company logo
     const logoUrl = 'https://i.postimg.cc/MpCNtPX6/Whats-App-Image-2023-12-27-at-4-24-00-PM.jpg';
+
+    // URL to open for image uploading
     const imageUploadUrl = 'https://postimages.org/';
-    
+
     const [anchorEl, setAnchorEl] = useState(null);
     const isMenuOpen = Boolean(anchorEl);
 
@@ -29,14 +32,16 @@ function Navbar({ onAddTire, onOpenImageUpload }) {
         <AppBar position="static" style={{ backgroundColor: 'white' }}>
             <Toolbar style={{ justifyContent: 'space-between', alignItems: 'center' }}>
 
+                {/* Hamburger Menu */}
                 <IconButton
-                    style={{ color: orange[500] }}
+                    style={{ color: 'orange' }}
                     edge="start"
                     onClick={handleMenuOpen}
                 >
                     <MenuIcon />
                 </IconButton>
 
+                {/* Menu Items */}
                 <Menu
                     anchorEl={anchorEl}
                     anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
@@ -46,16 +51,26 @@ function Navbar({ onAddTire, onOpenImageUpload }) {
                     onClose={handleMenuClose}
                 >
                     <MenuItem onClick={onAddTire}>
-                        <AddCircleIcon style={{ color: orange[500], marginRight: '10px' }} />
+                        <AddCircleIcon style={{ marginRight: '10px', color: 'orange' }} />
                         Add Tire
                     </MenuItem>
                     <MenuItem onClick={() => window.open(imageUploadUrl, '_blank')}>
-                        <LinkIcon style={{ color: orange[500], marginRight: '10px' }} />
+                        <LinkIcon style={{ marginRight: '10px', color: 'orange' }} />
                         Image Upload
+                    </MenuItem>
+                    <MenuItem onClick={onArchive}>
+                        <ArchiveIcon style={{ marginRight: '10px', color: 'orange' }} />
+                        Archive
                     </MenuItem>
                 </Menu>
 
+                {/* Company Logo */}
                 <img src={logoUrl} alt="Company Logo" style={{ maxHeight: '50px', minHeight: '20px' }} />
+
+                {/* Search Icon */}
+                <IconButton style={{ color: 'orange' }} onClick={onSearchTire}>
+                    <SearchIcon />
+                </IconButton>
 
             </Toolbar>
         </AppBar>

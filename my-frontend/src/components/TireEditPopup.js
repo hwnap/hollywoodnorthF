f95f2aea320e6ffe67f8eac2ba944742b2f105dd
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-03 15:27:54 -0500

    Tire Edit Fix 1

--- my-frontend/src/components/TireEditPopup.js
+++ my-frontend/src/components/TireEditPopup.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-// import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
-import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button } from '@mui/material';
+ import { Dialog, DialogTitle, DialogContent, DialogActions, TextField, Button, FormControl, InputLabel, Select, MenuItem } from '@mui/material';
 
 function TireEditPopup({ open, onClose, tire, onSave, isAdmin }) {
   const [tireData, setTireData] = useState({
@@ -51,7 +50,39 @@ function TireEditPopup({ open, onClose, tire, onSave, isAdmin }) {
         {/* Brand, Size, and other fields */}
         <TextField name="brand" label="Brand" fullWidth margin="dense" variant="standard" value={tireData.brand} onChange={handleChange} />
         {/* ...other input fields... */}
-        
+        <TextField name="size" label="Size" fullWidth margin="dense" variant="standard" value={tireData.size} onChange={handleChange} />
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="tread-condition-label">Tread Condition</InputLabel>
+          <Select labelId="tread-condition-label" name="treadCondition" value={tireData.treadCondition} label="Tread Condition" onChange={handleChange}>
+            {['60%', '65%', '70%', '75%', '80%', '85%', '90%', '95%', '100%'].map(percent => (
+              <MenuItem key={percent} value={percent}>{percent}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="season-label">Season</InputLabel>
+          <Select labelId="season-label" name="season" value={tireData.season} label="Season" onChange={handleChange}>
+            <MenuItem value="All Season">All Season</MenuItem>
+            <MenuItem value="Summer">Summer</MenuItem>
+            <MenuItem value="Winter">Winter</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="set-info-label">Tire Set</InputLabel>
+          <Select labelId="set-info-label" name="setInfo" value={tireData.setInfo} label="Tire Set" onChange={handleChange}>
+            <MenuItem value="Set of 2">Set of 2</MenuItem>
+            <MenuItem value="Set of 4">Set of 4</MenuItem>
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="location-label">Location</InputLabel>
+          <Select labelId="location-label" name="location" value={tireData.location} label="Location" onChange={handleChange}>
+            <MenuItem value="Toronto">Toronto</MenuItem>
+            <MenuItem value="Barrie">Barrie</MenuItem>
+            <MenuItem value="Sutton West">Sutton West</MenuItem>
+          </Select>
+        </FormControl>
+        <TextField name="imageUrls" label="Image URLs (comma separated)" fullWidth margin="dense" variant="standard" value={tireData.imageUrls.join(', ')} onChange={handleChange} helperText="Enter URLs separated by commas" />
         {/* Price field with admin check */}
         <TextField 
           name="price" 

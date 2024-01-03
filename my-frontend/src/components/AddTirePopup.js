26c59cf2272947e8eed6fc7ee2ddac5a119b24a5
ezacoantonio <126419692+ezacoantonio@users.noreply.github.com>
2024-01-02 22:08:45 -0500

    AddTirePopup update 2

--- my-frontend/src/components/AddTirePopup.js
+++ my-frontend/src/components/AddTirePopup.js
@@ -6,7 +6,7 @@ function AddTirePopup({ open, onClose, onAddTire }) {
     brand: '',
     size: '',
     treadCondition: '',
-    status: '',
+    status: '', // Added status field
     imageUrls: [],
     location: '',
     setInfo: '',
@@ -23,9 +23,6 @@ function AddTirePopup({ open, onClose, onAddTire }) {
   };
 
   const handleSubmit = () => {
-    // Log the tireData to see what's being sent
-    console.log('Submitting tire data:', tireData);
-
     onAddTire(tireData);
     onClose();
     setTireData({ brand: '', size: '', treadCondition: '', status: '', imageUrls: [], location: '', setInfo: '', season: '' }); // Reset form
@@ -43,6 +40,15 @@ function AddTirePopup({ open, onClose, onAddTire }) {
             {['10%', '20%', '30%', '40%', '50%', '60%', '70%', '80%', '90%', '100%'].map(percent => (
               <MenuItem key={percent} value={percent}>{percent}</MenuItem>
             ))}
+          </Select>
+        </FormControl>
+        <FormControl fullWidth margin="dense">
+          <InputLabel id="status-label">Status</InputLabel>
+          <Select labelId="status-label" name="status" value={tireData.status} label="Status" onChange={handleChange}>
+            <MenuItem value="posted">Posted</MenuItem>
+            <MenuItem value="not posted">Not Posted</MenuItem>
+            <MenuItem value="sold">Sold</MenuItem>
+            <MenuItem value="not sold">Not Sold</MenuItem>
           </Select>
         </FormControl>
         <FormControl fullWidth margin="dense">
